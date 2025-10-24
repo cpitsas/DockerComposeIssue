6a6ee6d1e0511bf3104cd7e044f1dda4205c4115
ValadisPitsas <cpitsas@logistic-i.gr>
2025-10-24 21:38:40 +0300

    example microservice setup

---
+++ Orders/DockerComposeIssue.Orders.Consumer/Program.cs
@@ -0,0 +1,7 @@
+using DockerComposeIssue.Orders.Consumer;
+
+var builder = Host.CreateApplicationBuilder(args);
+builder.Services.AddHostedService<Worker>();
+
+var host = builder.Build();
+host.Run();
